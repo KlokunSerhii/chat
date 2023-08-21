a1b44607ec88ed5547dd864d94cb935537cb1626
Serhii Klokun <klokuns@gmail.com>
2023-08-22 01:43:51 +0300

    ref

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -1,10 +1,17 @@
 import React, { useEffect, useState } from 'react';
 import io from 'socket.io-client';
-
-import { Container } from './Chat.styled';
 import { useLocation } from 'react-router-dom';
+
+import {
+  Container,
+  DivUser,
+  DivYou,
+  NameUser,
+  NameYou,
+  User,
+  You,
+} from './Chat.styled';
 import ChatForm from 'components/ChatForm/ChatForm';
-import Messerger from 'components/Messages/Messages';
 
 const socket = io.connect(
   'https://chat-back-end-6mf9.onrender.com'
@@ -40,17 +47,22 @@ const Chat = () => {
   return (
     <>
       <Container>
-        <Messerger messages={state} name={params.name} />
-
-        {/* <DivYou>
-        <NameYou></NameYou>
-        <You></You>
-      </DivYou>
-
-      <DivUser>
-        <NameUser></NameUser>
-        <User></User>
-      </DivUser> */}
+        {state.map(({ user, message }) => {
+          const you =
+            user.name.trim().toLowerCase() ===
+            params.name.trim().toLowerCase();
+          return you ? (
+            <DivYou>
+              <NameYou>{user.name}</NameYou>
+              <You>{message}</You>
+            </DivYou>
+          ) : (
+            <DivUser>
+              <NameUser>{user.name}</NameUser>
+              <User>{message}</User>
+            </DivUser>
+          );
+        })}
       </Container>
       <ChatForm onSubmit={addMessage} />
     </>
