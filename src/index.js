a9bc21e7a52d9cd78cd81792613780667002d048
Serhii Klokun <99805859+KlokunSerhii@users.noreply.github.com>
2023-08-28 09:20:13 +0000

    deploy

--- src/index.js
+++ src/index.js
@@ -11,13 +11,13 @@ const root = ReactDOM.createRoot(
   document.getElementById('root')
 );
 root.render(
-  // <React.StrictMode>
-  <BrowserRouter>
+  <React.StrictMode>
+  <BrowserRouter basename="/Chat-lite">
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <App />
       </PersistGate>
     </Provider>
   </BrowserRouter>
-  // </React.StrictMode>
+   </React.StrictMode>
 );
