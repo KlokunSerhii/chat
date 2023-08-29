e51e5f32d0c0d4a6b5cf2ebb457c49602569b9e5
Serhii Klokun <99805859+KlokunSerhii@users.noreply.github.com>
2023-08-29 07:48:56 +0000

    deploy

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { persistor, store } from 'redux/store';
 import { PersistGate } from 'redux-persist/integration/react';
@@ -12,12 +12,13 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-  <BrowserRouter>
+  <HashRouter basename='/'  >
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <App />
       </PersistGate>
     </Provider>
-  </BrowserRouter>
+  </HashRouter>
    </React.StrictMode>
 );
+// basename='/'
