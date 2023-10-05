9072847c425220bb65e2ea4708d54775b3ae8882
Serhii Klokun <klokuns@gmail.com>
2023-10-05 23:38:45 +0300

    lazy git commit -m lazy q git q git commit -m lazy git commit -m lazy

--- src/components/App.js
+++ src/components/App.js
@@ -1,85 +1,71 @@
 import { Route, Routes } from 'react-router-dom';
-import { useEffect } from 'react';
+import { Suspense, lazy, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-
-import ChatView from 'page/Profile/ChatPage.jsx/ChatPage';
-import RegisterPage from 'page/SingUp';
-import LoginPage from 'page/SingIn';
-import SharedLayout from './SharedLayout';
-import PrivateRoute from './PrivateRoute';
-import RestrictedRoute from './RestrictedRoute';
-import { useAuth } from 'hooks/auth';
 import { refreshUser } from 'redux/auth/operations';
 import { AppLoader } from './Loader/Loader';
-import stales from './App.module.css';
-import SigningChat from 'page/SingInChat/SigningChat';
-import ProfilePage from 'page/Profile/ProfilePage';
-import WelcomPage from 'page/Welcome';
+import PrivateRoute from '../routes/PrivateRoute';
+import RestrictedRoute from '../routes/RestrictedRoute';
+
+const WelcomePage = lazy(() => import('page/Welcome'));
+const ChatView = lazy(() => import('page/ChatPage/ChatPage'));
+const RegisterPage = lazy(() => import('page/SingUp'));
+const LoginPage = lazy(() => import('page/SingIn'));
+const SharedLayout = lazy(() => import('./SharedLayout'));
+const SigningChat = lazy(() => import('page/SingInChat/SigningChat'));
+const ProfilePage = lazy(() => import('page/Profile/ProfilePage'));
 
 const App = () => {
   const dispatch = useDispatch();
-  const { isRefreshing } = useAuth();
 
   useEffect(() => {
     dispatch(refreshUser());
   }, [dispatch]);
 
-  return isRefreshing ? (
-    <div className={stales.loader}>
-      <AppLoader />
-    </div>
-  ) : (
-    <Routes>
-      <Route path="/" element={<SharedLayout />}>
-        <Route index element={<WelcomPage />} />
-        <Route
-          path="login"
-          element={
-            <RestrictedRoute
-              redirectto="/join"
-              component={LoginPage}
-            />
-          }
-        />
-        <Route
-          path="register"
-          element={
-            <RestrictedRoute
-              redirectto="/join"
-              component={RegisterPage}
-            />
-          }
-        />
-        <Route
-          path="join"
-          element={
-            <PrivateRoute
-              redirectto="/login"
-              component={SigningChat}
-            />
-          }
-        />
-        <Route
-          path="chat/:id"
-          element={
-            <PrivateRoute
-              redirectto="/login"
-              component={ChatView}
-            />
-          }
-        />
-        <Route
-          path="profile"
-          element={
-            <PrivateRoute
-              redirectto="/login"
-              component={ProfilePage}
-            />
-          }
-        />
-        <Route path="*" element={<div>NotFound </div>} />
-      </Route>
-    </Routes>
+  return (
+    <Suspense fallback={<AppLoader />}>
+      <Routes>
+        <Route path="/" element={<SharedLayout />}>
+          <Route index element={<WelcomePage />} />
+          <Route
+            path="login"
+            element={
+              <RestrictedRoute
+                redirectto="/join"
+                component={LoginPage}
+              />
+            }
+          />
+          <Route
+            path="register"
+            element={
+              <RestrictedRoute
+                redirectto="/join"
+                component={RegisterPage}
+              />
+            }
+          />
+          <Route
+            path="join"
+            element={
+              <PrivateRoute redirectto="/" component={SigningChat} />
+            }
+          />
+          <Route
+            path="chat/:id"
+            element={
+              <PrivateRoute redirectto="/" component={ChatView} />
+            }
+          />
+          <Route
+            path="profile"
+            element={
+              <PrivateRoute redirectto="/" component={ProfilePage} />
+            }
+          />
+          <Route path="*" element={<div>NotFound </div>} />
+        </Route>
+      </Routes>
+    </Suspense>
   );
 };
 
--- src/page/ChatPage/ChatPage.jsx
+++ src/page/ChatPage/ChatPage.jsx
@@ -1,4 +1,4 @@
-import Chat from '../../../components/Chat';
+import Chat from '../../components/Chat';
 
 function ChatPage() {
   return (
